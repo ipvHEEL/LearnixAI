dad13ab696b8353ca51968f98d9662916ec6642a
Andrewy <andrew0rohov2005@gmail.com>
2026-01-21 08:18:50 +0300

    fix

--- service/user/user.py
+++
@@ -1,8 +0,0 @@
-#service/user/user.py
-class User:
-    def __init__(self, user_id: int, interests: list[str]):
-        self.user_id = user_id
-        self.interests = interests
-    
-    def has_interests(self) -> bool:
-        return len(self.interests > 0 )
