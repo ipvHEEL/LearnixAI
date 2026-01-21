633f33477211ce421f153fa24cf58ee445121574
Andrewy <andrew0rohov2005@gmail.com>
2026-01-21 20:28:31 +0300

    add docker containers

--- frontend/src/LoginForm.jsx
+++ frontend/src/LoginForm.jsx
@@ -26,7 +26,7 @@ function LoginForm() {
   try {
     const payload = { login: form.username, password: form.password };
 
-    const res = await fetch("http://127.0.0.1:8000/login", {
+    const res = await fetch("http://backend/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(payload)
